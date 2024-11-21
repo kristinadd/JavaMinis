c6d29cf3ab91af4be1e6340dc7bef6a6c264c9ff
kristinadd <kristina@productboard.com>
2024-11-21 20:25:17 +0100

    HashSet

--- HashSet/HashSetTesting.java
+++ HashSet/HashSetTesting.java
@@ -5,17 +5,11 @@ import java.util.HashSet;
 public class HashSetTesting {
   // A HashSet is a class in Java that implements the Set interface and is part of the java.util package. 
   // It represents a collection of unique elements, meaning it does not allow duplicate values.
-
   // A HashSet is backed by a HashMap, and it uses hashing to store and retrieve elements efficiently.
-
   // A HashSet does not allow duplicate elements. If you try to add a duplicate, the operation will be ignored.
-
   // The elements in a HashSet are not stored in the order they were added. The order is determined by the hash values.
-
   // A HashSet can store one null value because it treats null as a unique value.
-
   // The HashSet provides constant-time performance (O(1) on average) for common operations like add, remove, and contains.
-
   // Internally, a HashSet is backed by a HashMap, where the elements are stored as keys in the HashMap and a dummy object acts as the value.
 
   public static void main(String[] args) {
@@ -26,10 +20,10 @@ public class HashSetTesting {
     set.add("Cherry"); // duplicate; ignored 
 
     System.out.println(set.contains("Orange"));
-    System.out.println(set.contains("Browney"));
+    System.out.println(set.contains("Brrrr"));
 
     System.out.println(set.remove("Apple"));
-    System.out.println(set.remove("Browney"));
+    System.out.println(set.remove("Brrrr"));
 
     System.out.println(set.size());
 
