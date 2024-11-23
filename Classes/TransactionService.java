0e91ff2c6480bac69b84358ca9fadaa022875414
kristinadd <kristina@productboard.com>
2024-11-23 12:11:10 +0100

    Classes

--- Classes/TransactionService.java
+++ Classes/TransactionService.java
@@ -12,6 +12,10 @@ public class TransactionService {
     // account1.balance = 900.00;
     // System.out.println(account1.balance);
 
+    // Breaking Encapsulation
+    // Encapsulation is about hiding the internal details of a class and exposing only the necessary behavior through methods.
+    // When fields are public, you expose the internal state of the object directly, making it vulnerable to unintended changes.
+    // To prevent this, fields should be declared private and accessed or modified only through public methods. This allows you to control how the field is updated and enforce rules.
   }
   
 }
