21be347f89e394c429197aa62bc61c3237636be5
kristinadd <kristina@productboard.com>
2025-01-06 18:58:11 +0100

    Testing

--- Synchronized/SynchronizedTest.java
+++ Synchronized/SynchronizedTest.java
@@ -5,6 +5,27 @@ public class SynchronizedTest {
   // It ensures that only one thread at a time can execute a block of code or a method that is marked as synchronized. 
   // This prevents race conditions and ensures thread safety.
 
-  
+  // // // // // // // //
+
+  // Synchronized Methods
+  // Synchronization is applied to an entire method.
+  // The lock is acquired on the object instance (this) or the class (if the method is static).
+
+  // In this example:
+  // increment() and getCount() methods are synchronized.
+  // Only one thread can execute either of these methods at a time for a single Counter instance.
+
+  private int count = 0;
+
+  // Synchronized method
+  public synchronized void increment() {
+      count++;
+  }
+
+  public synchronized int getCount() {
+      return count;
+  }
+
+
 
 }
