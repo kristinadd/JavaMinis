2b7064dc83357215f6702aa28b22e9d902c94e57
kristinadd <kristina@productboard.com>
2025-01-06 18:54:58 +0100

    Testing

---
+++ Synchronized/SynchronizedTest.java
@@ -0,0 +1,10 @@
+package Synchronized;
+
+public class SynchronizedTest {
+  // The synchronized keyword in Java is used to control access to a shared resource in a multithreaded environment. 
+  // It ensures that only one thread at a time can execute a block of code or a method that is marked as synchronized. 
+  // This prevents race conditions and ensures thread safety.
+
+  
+
+}
