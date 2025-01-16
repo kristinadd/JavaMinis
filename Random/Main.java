1a66e74fda31d10f89b8643214c0b543397885a6
kristinadd <kristina@productboard.com>
2025-01-16 18:11:37 +0100

    Testing

--- Random/Main.java
+++ Random/Main.java
@@ -1,17 +1,29 @@
 package Random;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
   public static void main(String[] args) {
-    String[] names = {"Kristina", "Bobby", "Alice", "John"};
+    // String[] names = {"Kristina", "Bobby", "Alice", "John"};
+
+    // Random random = new Random();
+
+    // int randomIndex = random.nextInt(names.length);
+
+    // System.out.println(names[randomIndex]);
+
+    ArrayList<String> names = new ArrayList<>();
+    names.add("Kristina");
+    names.add("Bobby");
+    names.add("Lia");
+    names.add("Neo");
 
     Random random = new Random();
 
-    int randomIndex = random.nextInt(names.length);
+    int randomIndex = random.nextInt(names.size());
 
-    System.out.println(names[randomIndex]);
+    System.out.println(names.get(randomIndex));
 
-    
   }
 }
