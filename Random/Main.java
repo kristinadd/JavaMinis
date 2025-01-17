a609fc29b12871ac28dd003a6e47c83910c799d1
kristinadd <kristina@productboard.com>
2025-01-17 08:22:28 +0100

    Testing

--- Random/Main.java
+++ Random/Main.java
@@ -1,29 +1,39 @@
 package Random;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class Main {
   public static void main(String[] args) {
-    // String[] names = {"Kristina", "Bobby", "Alice", "John"};
-
-    // Random random = new Random();
-
-    // int randomIndex = random.nextInt(names.length);
-
-    // System.out.println(names[randomIndex]);
-
     ArrayList<String> names = new ArrayList<>();
     names.add("Kristina");
     names.add("Bobby");
     names.add("Lia");
     names.add("Neo");
 
-    Random random = new Random();
+    // for (int i=0; i<=names.size(); i++) {
+    //   Random random = new Random();
+    //   int randomIndex = random.nextInt(names.size());
+    //   System.out.println(names.get(randomIndex));
+    //   names.remove(randomIndex);
+    //   System.out.println(names);
+    // }
 
-    int randomIndex = random.nextInt(names.size());
+    // Random random = new Random();
 
-    System.out.println(names.get(randomIndex));
+    // while (!names.isEmpty()) {
+    //   int randomIndex = random.nextInt(names.size());
+    //   System.out.println(names.get(randomIndex));
+    //   names.remove(randomIndex);
+    // }
 
+    Collections.shuffle(names);
+    System.out.println(names);
+
+    // while (!names.isEmpty()) {
+    //   System.out.println(names);
+    //   names.remove(0);
+    // } 
   }
 }
