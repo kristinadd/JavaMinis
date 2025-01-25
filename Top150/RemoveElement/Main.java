b7d5580eab34ca4ebb547434e06d105296c34774
kristinadd <kristina@productboard.com>
2025-01-25 08:17:13 +0100

    150

--- Top150/RemoveElement/Main.java
+++ Top150/RemoveElement/Main.java
@@ -1,5 +1,40 @@
 package Top150.RemoveElement;
 
+import java.util.ArrayList;
+
 public class Main {
+  public static void main(String[] args) {
+    int[] nums = {0,1,2,2,3,0,4,2};
+    int val = 2;
+    removeElement(nums, val);
+  }
+
+
+  // public static int removeElement(int[] nums, int val) {
+  //   ArrayList<Integer> sanitizedList = new ArrayList<>();
+  //   for (int i=0; i<nums.length; i++) {
+  //     if (nums[i] != val) {
+  //       sanitizedList.add(nums[i]);
+  //     }
+  //   }
+  //   System.out.println(sanitizedList.size());
+  //   System.out.println(sanitizedList);
+  //   return sanitizedList.size();
+  //   }
+
+
+  // Solution
+    public static int removeElement(int[] nums, int val) {
+      int k = 0;
+  
+      for (int i = 0; i < nums.length; i++) {
+          if (nums[i] != val) {
+              nums[k] = nums[i];
+              k++; 
+          }
+      }
+  
+      return k; 
+  }
   
 }
