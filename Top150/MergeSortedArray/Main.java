acd6fdadf45ad3ef6fa38e5376d2349fb9de8435
kristinadd <kristina@productboard.com>
2025-01-26 18:19:06 +0100

    150

--- Top150/MergeSortedArray/Main.java
+++ Top150/MergeSortedArray/Main.java
@@ -9,7 +9,7 @@ public class Main {
     int[] array2 = {2,5,6};
     int n = 3;
 
-    merge(array1,m,array2,n);
+    mergeEfficiently(array1,m,array2,n);
   } 
 
   public static int[] merge(int[] array1, int m, int[] array2, int n) {
@@ -19,6 +19,25 @@ public class Main {
     Arrays.sort(array1);
     System.out.println(Arrays.toString(array1));
     return array1;
+  }
+// merge in-place
+  public static int[] mergeEfficiently(int[] nums1, int m, int[] nums2, int n) {
+    int i = m-1;
+    int j = n-1;
+    int k = (m+n)-1;
+
+    while (i >= 0 && j >= 0) {
+      if (nums1[i] > nums2[j]) {
+        nums1[k] = nums1[i];
+        i --;
+      } else {
+        nums1[k] = nums2[j];
+        j --;
+      }
+      k --;
+    }
+    System.out.println(Arrays.toString(nums1));
+    return nums1;
   }
 }
 
--- Top150/RemoveElement/Main.java
+++ Top150/RemoveElement/Main.java
@@ -1,6 +1,7 @@
 package Top150.RemoveElement;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
@@ -24,17 +25,17 @@ public class Main {
 
 
   // Solution
-    public static int removeElement(int[] nums, int val) {
-      int k = 0;
-  
-      for (int i = 0; i < nums.length; i++) {
-          if (nums[i] != val) {
-              nums[k] = nums[i];
-              k++; 
-          }
-      }
-  
-      return k; 
+  public static int removeElement(int[] nums, int val) {
+    int k = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++; 
+        }
+    }
+    System.out.println(Arrays.toString(nums));
+    System.out.println(k);
+    return k; 
   }
-  
 }
